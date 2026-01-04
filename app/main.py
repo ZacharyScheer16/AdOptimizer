9c6ad8d59f321424b6b6a69010c80ccac78b161b
Zachary Scheer <zscheer00@gmail.com>
2026-01-04 16:12:29 -0700

    Upload data post api trsnslates into json data

--- app/main.py
+++ app/main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from .model import run_clustering
+import pandas as pd
+import io
 
 app = FastAPI()
 @app.get("/")
 async def home():
     return {"message": "Welcome to the AdOptimizer API!"}
+
+
+@app.post("/upload-logistics")
+async def upload_data(file: UploadFile = File(...)):
+    # 1. Read the raw bytes
+    contents = await file.read()
+    
+    # 2. Turn bytes into a table (DataFrame)
+    # We use io.BytesIO(contents) to make the bytes look like a file
+    df = pd.read_csv(io.BytesIO(contents))
+    
+    # 3. Use your model!
+    result = run_clustering(df)
+    
+    return {
+        "filename": file.filename,
+        "ad_analyzed": len(df),
+        "analysis": result
+    }
